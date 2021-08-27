6cab6e9674b5568e9627c27e3e4ed847c007abc3
idomagner <36795414+idomagner@users.noreply.github.com>
2021-08-27 11:57:13 +0300

    homescreen added pageview

--- lib/screens/login/homescreen.dart
+++ lib/screens/login/homescreen.dart
@@ -1,7 +1,7 @@
 library authantication;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+/*
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -20,6 +20,52 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Center(
         child: Text("THIS IS HOME"),
       ),
+    );
+  }
+}
+*/
+
+
+/// This is the main application widget.
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        body: const MyStatelessWidget(),
+      ),
+    );
+  }
+}
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController(initialPage: 0);
+    return PageView(
+      /// [PageView.scrollDirection] defaults to [Axis.horizontal].
+      /// Use [Axis.vertical] to scroll vertically.
+      scrollDirection: Axis.horizontal,
+      controller: controller,
+      children: const <Widget>[
+        Center(
+          child: Text('שלום!'),
+        ),
+        Center(
+          child: Text('זה אפליקציה שתתקן לך חרדה'),
+        ),
+        Center(
+          child: Text('ביי!'),
+        )
+      ],
     );
   }
 }
