089a6a17e265fb9ec0feb01dabba0fe0806a822f
alonkit <alon.kitin@gmail.com>
2021-09-08 08:25:51 +0300

    caretakers join us

--- lib/screens/caretaker/home.dart
+++ lib/screens/caretaker/home.dart
@@ -1,4 +1,6 @@
 library home;
+import 'dart:math';
+
 import 'package:application/screens/map/map.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -22,95 +24,137 @@ class _HomeState extends State<CareHome>{
   TextEditingController password = TextEditingController();
 
 
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('caretaker!'),
-      ),
-      drawer: Builder(builder: (context)=> Drawer(
-        child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: Text('CareTaker'),
-              ),
-              ListTile(
-                title: const Text("התנתק"),
-                onTap: () {
-                  Future<void> _signOut() async {
-                    await FirebaseAuth.instance.signOut();
-                  }
-                  // Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  //     builder: (BuildContext context) => Login()));
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
-                  ///Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: const Text("מפה!"),
-                onTap: () {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(),
+          body: StreamBuilder<List<QueryDocumentSnapshot>>(
+            stream: FirebaseFirestore.instance.collection('users').where('caretakeId',isEqualTo: AuthRepository.instance().user?.uid).snapshots().map( (value) => value.docs),
+            builder: (BuildContext context, AsyncSnapshot<List<QueryDocumentSnapshot>> snapshot) {
+              // ...
+              if (snapshot.hasData) {
+                final List<QueryDocumentSnapshot> data = snapshot.data!;
+                print(data);
+                print(AuthRepository.instance().user?.uid);
+                return SizedBox(
 
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (BuildContext context) => MapPage()));
-                  ///Navigator.push(context, MaterialPageRoute(builder: (context) => Login(isInit: false)));
-                  ///Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                selected: createPass,
-                title: const Text("סיסמא"),
-                onTap: () {
-                      setState(() {
-                        createPass= !createPass;
-                      });
-                },
-              ),
-              if(createPass) Row(mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                // Container(color: Colors.green, width: 10,height: 10,),
-                  Container(width: MediaQuery.of(context).size.width*0.5,
-                      child: TextFormField(
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly
-                        ], //
-                        controller: password,
-                        decoration: InputDecoration(
-                          hintText: "PASSWORD...",
-                        ),
-                      )
-                  ),
-                 Container(
-                     height:  MediaQuery.of(context).size.width*0.2,
-                     width:  MediaQuery.of(context).size.width*0.2,
-                     child: TextButton(
-                      onPressed: () {
-                        final String pass = password.text.trim();
-                        FirebaseFirestore.instance.collection("caretakers").doc(AuthRepository.instance().user?.uid).set({
-                          'usedId' : pass
-                        },SetOptions(merge: true));
-                        setState(() {
-                          createPass=!createPass;
-                        });
+                  height: 200.0,
+                  child: ListView.separated(
+                      itemBuilder: (context, index) {
+                        final Map<String, dynamic> item = data[index].data()!;
+                        // final userId = data[index].id;
+                        return ListTile(
+                          title: Text(item['name'],
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          leading: IconButton(
+                            icon: Icon(Icons.thumb_down),
+                            onPressed: () {
+                            },
+                          ),
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete_outline),
+                            onPressed: () {
+                            },
+                          ),
+                        );
                       },
-                      child: Icon(Icons.view_in_ar),
-                    )
-                ),
-                ],
-              )
+                      separatorBuilder: (_, __) => Divider(),
+                      itemCount: data.length),
+                );
+              }
+              return CircularProgressIndicator();
+            },
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.person_add) ,
+            onPressed: (){FirebaseFirestore.instance.collection("caretakers").doc(AuthRepository.instance().user?.uid).set({
+              'time' : FieldValue.serverTimestamp()
+            },SetOptions(merge: true));},
+          ),
+          drawer: Builder(builder: (context)=> Drawer(
+            child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  DrawerHeader(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                      ),
+                      child: Text('CareTaker'),
+                  ),
+                  ListTile(
+                    title: const Text("התנתק"),
+                    onTap: () {
+                      Future<void> _signOut() async {
+                        await FirebaseAuth.instance.signOut();
+                      }
+                      // Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      //     builder: (BuildContext context) => Login()));
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
+                      ///Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: const Text("מפה!"),
+                    onTap: () {
 
-            ]
-        ),
-      ),
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (BuildContext context) => MapPage()));
+                      ///Navigator.push(context, MaterialPageRoute(builder: (context) => Login(isInit: false)));
+                      ///Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    selected: createPass,
+                    title: const Text("סיסמא"),
+                    onTap: () {
+                          setState(() {
+                            createPass= !createPass;
+                          });
+                    },
+                  ),
+                  if(createPass) Row(mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                    // Container(color: Colors.green, width: 10,height: 10,),
+                      Container(width: MediaQuery.of(context).size.width*0.5,
+                          child: TextFormField(
+                            initialValue: Random().nextInt(999999).toString(),
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly
+                            ], //
+                            controller: password,
+                            decoration: InputDecoration(
+                              hintText: "PASSWORD...",
+                            ),
+                          )
+                      ),
+                     Container(
+                         height:  MediaQuery.of(context).size.width*0.2,
+                         width:  MediaQuery.of(context).size.width*0.2,
+                         child: TextButton(
+                          onPressed: () {
+                            final String pass = password.text.trim();
+                            FirebaseFirestore.instance.collection("caretakers").doc(AuthRepository.instance().user?.uid).set({
+                              'usedId' : pass
+                            },SetOptions(merge: true));
+                            setState(() {
+                              createPass=!createPass;
+                            });
+                          },
+                          child: Icon(Icons.view_in_ar),
+                        )
+                    ),
+                    ],
+                  )
 
-    )
-    )
+                ]
+            ),
+          ),)
+
+
+        )
     );
 
   }
