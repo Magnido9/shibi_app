cd5a5b8cf6615bc81e344b82f8fca1c645434928
alonkit <alon.kitin@gmail.com>
2021-10-11 23:35:24 +0300

    more stuff

--- lib/screens/expo1/thoughts.dart
+++ lib/screens/expo1/thoughts.dart
@@ -198,7 +198,13 @@ class _thought1_state extends State<thought1_1> {
                       wlist.add(Positioned(
                           top: offsets[i].dy,
                           left: offsets[i].dx,
-                          child: Baloon(angle:angles[i],diameter: diameters[i],text:widget.thoughts[i],color: i%4,)
+                          child: GestureDetector(
+                            child: Baloon(id:i,angle:angles[i],diameter: diameters[i],text:widget.thoughts[i],color: i%4,),
+                            onTap: (){Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => _BallonPage()),
+                            );},
+                          )
                       )) ;
                     }
                     return Stack(children:
@@ -217,46 +223,145 @@ class _thought1_state extends State<thought1_1> {
   }
 }
 
+class _BallonPage extends StatefulWidget{
+  @override
+  _ballonState createState() => _ballonState();
+}
+
+class _ballonState extends State<_BallonPage>{
+  TextEditingController emailController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context){
+    double width =MediaQuery.of(context).size.width;
+    double height =MediaQuery.of(context).size.height;
+    return Scaffold(
+
+      body: Stack(
+        children: [
+          Positioned(
+              bottom:height*0.8,
+              right: width*0.8,
+              child: Baloon(
+                id:1,
+                angle:0,
+                diameter:width*0.5,
+                color:1,
+                text:'',
+
+              )),
+          Positioned(
+              bottom:height*0.8,
+              left: width*0.7,
+              child: Baloon(
+                id:1,
+                angle:0.1,
+                diameter:width*0.6,
+                color:3,
+                text:'',
+
+              )),
+          Positioned(
+              top:height*0.6,
+              left: width*0.7,
+              child: Baloon(
+                id:1,
+                angle:0,
+                diameter:width*0.8,
+                color:2,
+                text:'',
+
+              )),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              padding: EdgeInsets.only(top:height*0.1),
+              child: Baloon(
+                  id:1,
+                  angle:0,
+                  diameter:width*0.95,
+                  color:1,
+                  text:'',)
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 class Baloon extends StatelessWidget{
   Baloon({
     required this.color,
     required this.diameter,
     required this.angle,
     required this.text,
+    required this.id,
+    this.secondery='',
+    this.chosen=false,
   });
+  final bool chosen;
+  final int id;
+  final String? secondery;
   final String text;
   final double diameter;
   final double angle;
   final int color;
-  final colors=[Color(0xffE0CBDD),Color(0xffDEEEF3),Color(0xffCFC781).withOpacity(0.26),Color(0xff81CF8D).withOpacity(0.26),];
+  final colors=[Color(0xffDEEEF3),Color(0xffDEEEF3),Color(0xffCFC781).withOpacity(0.26),Color(0xff81CF8D).withOpacity(0.26),];
 
 
   @override
   Widget build(BuildContext context){
-    return Container(height: diameter, width: diameter, child: Stack(
+    return Container(height: diameter, width: diameter,
+      child: Stack(
       children: [
-        Center(
-          child: Container(
-            width: 0.8*diameter,
-            child: Text(text,
-              textAlign: TextAlign.right,
-              textDirection: TextDirection.rtl,
-              style: TextStyle(
-                fontFamily: "Assistant",
-                fontWeight: FontWeight.w700,
-                color: Color(0xff35258A),
-              fontSize: 0.1*diameter,
-
-              )
-              ,),
+        if(chosen) Container(
+          width: diameter,
+          height: diameter*0.95,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.blue)
           ),
         ),
         Transform.rotate(
           angle: angle,
-          child: ImageColorSwitcher(height: diameter, width: diameter, imagePath: 'images/expo/baloon.png', main: Color(0xff000000),second: Color(0xff95DEF4),color: colors[color],),
+          child: ImageColorSwitcher(height: diameter, width: diameter, imagePath: 'images/expo/baloon.png', main: Color(0xff000000),second: Color(0xff95DEF4),color: colors[color%4],),
 
-        )
+        ),
+        Center(
+          child: Container(
+            width: 0.8*diameter,
+            child: RichText(
+              textDirection: TextDirection.rtl,
+
+              text: TextSpan(
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+                children: <TextSpan>[
+//
+                  TextSpan(
+                      text: text,
+                    style: TextStyle(
+                    color: Color(0xff35258a),
+                    fontSize: 0.1*diameter,
+                    fontFamily: "Assistant",
+                    fontWeight: FontWeight.w700,
+                  ),),
+                  TextSpan(
+                      text: secondery,
+                      style:TextStyle(
+                        fontSize: 14,
+                        fontFamily: "Assistant",
+                      ),),
+                ],
+              ),
+            ),
+          ),
+        ),
       ],
     ),);
   }
 }
+
